373c09a51b14f9a4097d8a6d5d9e513f6c0fbf97
DiegoAsmat12 <diegoas1205@gmail.com>
2022-02-14 11:30:23 -0500

    Primer Ejercicio de fundamentos

---
+++ PrimerPrograma.java
@@ -0,0 +1,11 @@
+public class PrimerPrograma{
+    public static void main ( String args[] ){
+        String nombre = "Diego Asmat";
+        int edad = 22;
+        String ciudadNatal = "Moche, Trujillo, La Libertad";
+
+        System.out.println("Mi nombre es "+nombre);
+        System.out.println("Tengo "+edad+" años de edad.");
+        System.out.println("Mi ciudad natal es: "+ciudadNatal);
+    }
+}
